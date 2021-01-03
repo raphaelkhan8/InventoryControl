514af3031388d72f2526c605c65fe818417c4a8b
Raphael Khan <raphaelkhan8@gmail.com>
2021-01-03 15:24:34 -0600

    removed constructor as Part is abstract class

--- src/model/Part.java
+++ src/model/Part.java
@@ -7,14 +7,6 @@ public abstract class Part {
     private int stock;
     private int min;
     private int max;
-    public Part(int id, String name, double price, int stock, int min, int max) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.min = min;
-        this.max = max;
-    }
 
     /// Setters ///
     public void setId(int id) { this.id = id; }
