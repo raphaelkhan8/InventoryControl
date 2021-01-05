754eb58962a55ea82c4d9c96b914114c314eb394
Raphael Khan <raphaelkhan8@gmail.com>
2021-01-04 21:40:44 -0600

    added controller for Add Part form

---
+++ src/controller/AddPartController.java
@@ -0,0 +1,68 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class AddPartController {
+    @FXML
+    private RadioButton InHouseRadioButton;
+
+    @FXML
+    private ToggleGroup RadioButton;
+
+    @FXML
+    private RadioButton OutsourcedRadioButton;
+
+    @FXML
+    private TextField NameAddPartText;
+
+    @FXML
+    private TextField InventoryAddPartText;
+
+    @FXML
+    private TextField PriceCostAddPartText;
+
+    @FXML
+    private TextField IDAddPartText;
+
+    @FXML
+    private TextField MaxAddPartText;
+
+    @FXML
+    private TextField MinAddPartText;
+
+    @FXML
+    private Button CancelButton;
+
+    @FXML
+    private Button SaveButton;
+
+    @FXML
+    private TextField IDAddPartText1;
+
+    @FXML
+    void InHouseHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OutsourcedHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelPart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void savePart(ActionEvent event) {
+
+    }
+
+}
+
