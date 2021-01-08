8179a66284f1040e89964a09dfe99537f6a81aff
Raphael Khan <raphaelkhan8@gmail.com>
2021-01-07 19:54:52 -0600

    implemented controller for Modify Part view

--- src/controller/ModifyPartController.java
+++ src/controller/ModifyPartController.java
@@ -1,2 +1,69 @@
-package controller;public class ModifyPartController {
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ModifyPartController {
+
+    @FXML
+    private RadioButton InHouseRadioButton;
+
+    @FXML
+    private ToggleGroup RadioButton;
+
+    @FXML
+    private RadioButton OutsourcedRadioButton;
+
+    @FXML
+    private TextField NameModifyPartText;
+
+    @FXML
+    private TextField InventoryModifyPartText;
+
+    @FXML
+    private TextField PriceCostModifyPartText;
+
+    @FXML
+    private TextField IDModifyPartText;
+
+    @FXML
+    private TextField MaxModifyPartText;
+
+    @FXML
+    private TextField MinModifyPartText;
+
+    @FXML
+    private Button CancelButton;
+
+    @FXML
+    private Button SaveButton;
+
+    @FXML
+    private TextField IDModifyPartText1;
+
+    @FXML
+    void InHouseHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OutsourcedHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelPart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void savePart(ActionEvent event) {
+
+    }
+
 }
+
