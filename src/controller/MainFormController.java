f4f96b1a136dc7cfd188ff13b70c51ed34825789
Raphael Khan <raphaelkhan8@gmail.com>
2021-01-09 14:18:51 -0600

    removed unecessary imports

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,8 +1,6 @@
 package controller;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
