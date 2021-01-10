6f7e30682c1516456c47e39aa7cc8ff6e0dd6bb4
Raphael Khan <raphaelkhan8@gmail.com>
2021-01-09 19:36:21 -0600

    all available parts display in Add Product form

--- src/controller/AddProductController.java
+++ src/controller/AddProductController.java
@@ -1,23 +1,31 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import controller.AddPartController;
+import model.Inventory;
+import model.Part;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AddProductController {
+public class AddProductController implements Initializable {
 
     Stage stage;
     Parent scene;
+
+    // Container for parts added to product
+    private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
 
     @FXML
     private TextField NameAddProductText;
@@ -47,7 +55,7 @@ public class AddProductController {
     private Button AddProductSaveButton;
 
     @FXML
-    private TableView<?> PartChoicesAddTable;
+    private TableView<Part> PartChoicesAddTable;
 
     @FXML
     private TableColumn<?, ?> ChoicesTableAddPartID;
@@ -62,19 +70,19 @@ public class AddProductController {
     private TableColumn<?, ?> ChoicesTableAddPricePerUnit;
 
     @FXML
-    private TableView<?> AssociatedPartsTable;
+    private TableView<Part> AssociatedPartsTable;
 
     @FXML
-    private TableColumn<?, ?> removePartID;
+    private TableColumn<?, ?> AssociatedPartID;
 
     @FXML
-    private TableColumn<?, ?> removePartName;
+    private TableColumn<?, ?> AssociatedPartName;
 
     @FXML
-    private TableColumn<?, ?> removeInventoryLevel;
+    private TableColumn<?, ?> AssociatedInventoryLevel;
 
     @FXML
-    private TableColumn<?, ?> removePricePerUnit;
+    private TableColumn<?, ?> AssociatedPricePerUnit;
 
     @FXML
     private Button removeAssociatedPartButton;
@@ -98,7 +106,15 @@ public class AddProductController {
 
     @FXML
     void lookupPart(MouseEvent event) {
-
+        String partToSearch = SearchPartText.getText();
+        ObservableList<Part> foundPartList;
+        foundPartList = Inventory.lookupPart(partToSearch);
+        // if searched Part isn't found, alert the user
+        if(foundPartList.isEmpty()) {
+            Inventory.alertMessage("Error", "Part not found", partToSearch + " was not found :(");
+        } else {
+            PartChoicesAddTable.setItems(foundPartList);
+        }
     }
 
     @FXML
@@ -111,4 +127,20 @@ public class AddProductController {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Populate All Parts table
+        PartChoicesAddTable.setItems(Inventory.getAllParts());
+        ChoicesTableAddPartID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        ChoicesTableAddPartName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ChoicesTableAddInventoryLevel.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        ChoicesTableAddPricePerUnit.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        // Populate Associated Parts table
+        AssociatedPartsTable.setItems(associatedParts);
+        AssociatedPartID.setCellValueFactory(new PropertyValueFactory<>("id"));;
+        AssociatedPartName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        AssociatedInventoryLevel.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        AssociatedPricePerUnit.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
 }
